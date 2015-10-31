e73c0f16683d1dfbd82d9c65f46d44e827aab108
呆萌诺 <daimengnuo@daimengnuodemacbook-air.local>
2015-10-31 19:43:53 +0800

    1.0项目初始化（导入图片，mvc结构）

---
+++ OCSina/OCSina/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCSina
+//
+//  Created by 呆萌诺 on 15/10/31.
+//  Copyright © 2015年 呆萌诺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
